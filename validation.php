eb36fd30d061cd3ffa2b51fdc89fc19a9fea58b8
Shreya Jain <shreya190704@keshav.du.ac.in>
2022-09-13 23:39:39 +0530

    Update validation.php

--- validation.php
+++ validation.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-$con=mysqli_connect('localhost','root');
-mysqli_select_db($con,'session');
+$con=mysqli_connect('sql213.epizy.com','epiz_32582075','n7LhsDfL8SX');
+mysqli_select_db($con,'epiz_32582075_session');
 $name=$_POST['user'];
 $pass=$_POST['password'];
 $q=" SELECT * from signup where name ='$name' and password='$pass'";
@@ -14,9 +14,12 @@ if($num==1)
 }
     
 else{
-    
-    header('location:sign-up.php');
     echo"<script>alert('Please register yourself')</script>";
+    $URL="http://logintest.rf.gd/sign-up.php";
+    echo ("<script>location.href='$URL'</script>");
+
+    
 }
 mysqli_close($con);
 ?>
+//infinityfree
